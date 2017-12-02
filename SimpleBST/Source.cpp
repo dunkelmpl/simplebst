75f8383e3a09176707342143d4478c74cb970038
Pavel Martynau <dunkel@tut.by>
2017-12-02 15:43:01 -0800

    Add project files.

---
+++ SimpleBST/Source.cpp
@@ -0,0 +1,300 @@
+#define _CRTDBG_MAP_ALLOC  
+#include <stdlib.h>  
+#include <crtdbg.h> 
+
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct BSTNode {
+    BSTNode(int value) : value(value), left(nullptr), right(nullptr) {}
+
+    int value;
+    BSTNode *left;
+    BSTNode *right;
+};
+
+typedef enum {PREORDER, INORDER, POSTORDER} Traversal;
+
+class SimpleBST {
+public:
+    SimpleBST() : top(nullptr) {}
+    SimpleBST(vector<int> input);
+    virtual ~SimpleBST();
+
+protected:
+    void disposeNode(BSTNode *node);
+
+public:
+    BSTNode* searchNode(int value);
+    void addNode(int value);
+    void deleteNode(int value);
+
+    void BFS(Traversal traversal);
+    void DFS();
+
+protected:
+    BSTNode* searchChildNode(BSTNode *parent, int value);
+    void addChildNode(BSTNode *parent, int value);
+    void deleteChildNode(BSTNode *parent, BSTNode *current, int value);
+    BSTNode* findNearestSuccessor(BSTNode* current, BSTNode** parent);
+
+protected:
+    void BFSChild(BSTNode* parent, Traversal traversal);
+
+private:
+    BSTNode *top;
+};
+
+SimpleBST::SimpleBST(vector<int> input) : SimpleBST()
+{
+    for (auto &item : input) {
+        this->addNode(item);
+    }
+}
+
+SimpleBST::~SimpleBST()
+{
+    disposeNode(top);
+}
+
+void SimpleBST::disposeNode(BSTNode *node)
+{
+    if (!node) {
+        return;
+    }
+
+    disposeNode(node->left);
+    disposeNode(node->right);
+
+    delete node;
+}
+
+BSTNode* SimpleBST::searchNode(int value)
+{
+    return searchChildNode(top, value);
+}
+
+BSTNode* SimpleBST::searchChildNode(BSTNode* parentNode, int value)
+{
+    if (!parentNode) {
+        return nullptr;
+    }
+
+    if (parentNode->value == value) {
+        return parentNode;
+    }
+
+    return (parentNode->value < value)
+        ? searchChildNode(parentNode->left, value)
+        : searchChildNode(parentNode->right, value);
+}
+
+void SimpleBST::addNode(int value)
+{
+    if (!top) {
+        top = new BSTNode(value);
+
+        return;
+    }
+
+    addChildNode(top, value);
+}
+
+void SimpleBST::addChildNode(BSTNode *parent, int value)
+{
+    if (!parent) {
+        throw invalid_argument("Parent node cannot be null");
+    }
+
+    if (value < parent->value) {
+        if (parent->left) {
+            addChildNode(parent->left, value);
+        }
+        else {
+            parent->left = new BSTNode(value);
+        }
+    }
+    else {
+        if (parent->right) {
+            addChildNode(parent->right, value);
+        }
+        else {
+            parent->right = new BSTNode(value);
+        }
+    }
+}
+
+void SimpleBST::deleteNode(int value)
+{
+    deleteChildNode(nullptr, top, value);
+}
+
+void SimpleBST::deleteChildNode(BSTNode *parent, BSTNode *current, int value)
+{
+    if (!current) {
+        stringstream s;
+        s << "Cannot delete " << value << " node. Node not found.";
+        throw out_of_range(s.str());
+    }
+
+    if (current->value == value) {
+        if (current->left || current->right) { //any child
+            if (!current->left || !current->right) { //one child
+                BSTNode* child = current->left ? current->left : current->right;
+
+                if (parent) {
+                    if (parent->left == current) {
+                        parent->left = child;
+                    }
+                    else {
+                        parent->right = child;
+                    }
+                }
+                else {
+                    top = child;
+                }
+
+                delete current;
+            }
+            else { //two children
+                BSTNode* nearestSuccessorParent = current;
+                BSTNode* nearestSuccessor = findNearestSuccessor(current, &nearestSuccessorParent);
+                current->value = nearestSuccessor->value;
+
+                if (nearestSuccessorParent->left == nearestSuccessor) {
+                    nearestSuccessorParent->left = nullptr;
+                }
+                else {
+                    nearestSuccessorParent->right = nullptr;
+                }
+
+                delete nearestSuccessor;
+            }
+        }
+        else { //no children, i.e. leaf
+            if (parent) {
+                if (parent->left == current) {
+                    parent->left = nullptr;
+                }
+                else {
+                    parent->right = nullptr;
+                }
+            }
+            else {
+                top = nullptr;
+            }
+
+            delete current;
+        }
+    }
+    else {
+        deleteChildNode(current, value < current->value ? current->left : current->right, value);
+    }
+}
+
+BSTNode* SimpleBST::findNearestSuccessor(BSTNode* current, BSTNode** parent)
+{
+    if (!current || !current->right) {
+        throw logic_error("Error: cannot find nearest greater. Invalid current node");
+    }
+
+    *parent = current;
+    current = current->right;
+
+    while (current->left) {
+        *parent = current;
+        current = current->left;
+    }
+
+    return current;
+}
+
+void SimpleBST::BFS(Traversal traversal)
+{
+    BFSChild(top, traversal);
+    cout << endl;
+}
+
+void SimpleBST::BFSChild(BSTNode* parent, Traversal traversal)
+{
+    if (!parent) {
+        return;
+    }
+
+    if (traversal == PREORDER) {
+        cout << parent->value << ' ';
+
+        BFSChild(parent->left, traversal);
+        BFSChild(parent->right, traversal);
+    }
+    else if (traversal == INORDER) {
+        BFSChild(parent->left, traversal);
+
+        cout << parent->value << ' ';
+
+        BFSChild(parent->right, traversal);
+    }
+    else { //POSTORDER
+        BFSChild(parent->left, traversal);
+        BFSChild(parent->right, traversal);
+
+        cout << parent->value << ' ';
+    }
+}
+
+void SimpleBST::DFS()
+{
+    if (!top) {
+        return;
+    }
+
+    queue<BSTNode*> nodesQueue;
+
+    nodesQueue.push(top);
+
+    while (!nodesQueue.empty()) {
+        BSTNode* current = nodesQueue.front();
+        nodesQueue.pop();
+
+        cout << current->value;
+
+        if (current->left) {
+            nodesQueue.push(current->left);
+        }
+
+        if (current->right) {
+            nodesQueue.push(current->right);
+        }
+    }
+}
+
+int main()
+{
+    {
+        SimpleBST bst({479, 359, 465, 963, 706, 300, 320});
+
+        bst.BFS(INORDER);
+        bst.BFS(PREORDER);
+        bst.BFS(POSTORDER);
+
+        cout << endl;
+
+        bst.deleteNode(359);
+        bst.deleteNode(320);
+        bst.deleteNode(479);
+
+        bst.BFS(INORDER);
+        bst.BFS(PREORDER);
+        bst.BFS(POSTORDER);
+
+    }
+
+    _CrtDumpMemoryLeaks();
+
+    return 0;
+}
